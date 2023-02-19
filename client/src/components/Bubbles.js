bc1667f78a9440e3c64ed65825100ae06c6b07ca
Akashdeep Singh <akash02ita@gmail.com>
2023-02-19 10:06:58 -0700

    client: allow highlight and cancellation of bubble selection

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import bubbles from "../util/bubbles";
 
+
 const Bubbles = (props) => {
+  const [selections, setSelections] = useState([]);
+
+  const handleSelection = (theme) => {
+    console.log(selections);
+    if (selections.includes(theme)) {
+      setSelections([]);
+      props.updateChoice(null);
+      return;
+    }
+    setSelections([theme]);
+    props.updateChoice(theme);
+  }
+
   return (
     <div className="row bubbles">
       {bubbles.map((bubble) => {
-        const { theme } = bubble;
-        return <span onClick={() => props.updateChoice(theme)} className="bubble">{theme}</span>;
+        const { theme, selected } = bubble;
+        if (selections.includes(theme)) {
+          console.log("theme is true", theme);
+          return <span style={{ color: "red" }} onClick={() => handleSelection(theme)} className="bubble">{theme}</span>
+        };
+        return <span onClick={() => handleSelection(theme)} className="bubble">{theme}</span>;
       })}
     </div>
   );
