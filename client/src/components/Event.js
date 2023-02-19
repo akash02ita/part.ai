51c410ce5d025aa02e44d4fd507289608051db1e
Akashdeep Singh <akash02ita@gmail.com>
2023-02-18 23:37:51 -0700

    client: retrieve geocode coordinatates from address this is to be used to add tags in gmaps. .env file also added forcibly

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,5 +1,46 @@
+import JSONdata from '../data/data.json'
+import Geocode from "react-geocode";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+const apiKey = process.env.REACT_APP_YOUR_GMAPS_KEY;
+// set Google Maps Geocoding API for purposes of quota management. Its optional but recommended.
+Geocode.setApiKey(apiKey);
+Geocode.enableDebug();
 
 const Event = () => {
+    const data = JSONdata;
+    // extract event-name from urs parama
+    const { name } = useParams();
+    // fetch all values
+    const { theme, description, bannerURL, address, attending, start, end } = data[name];
+
+    const [coord, setCoord] = useState(null);
+    useEffect(() => {
+        if (!address) {console.log("Address is null skipping it"); return;}
+        Geocode.fromAddress(address).then(
+            (response) => {
+                const { lat, lng } = response.results[0].geometry.location;
+                console.log(`Found coordinates for address ${address}:`, lat, lng);
+                setCoord([lat, lng]);
+            },
+            (error) => {
+                console.error(error);
+                console.log(`NO coordinates for address ${address}:`);
+            }
+        )
+    }, [address])
+
+
+    const renderMap = () => {
+        if (!coord) {
+            console.log("Coordinates not found so no map will be rendered");
+            return <></>
+        }
+
+        // return map at given coord
+    }
+
     return (
         <div> Event component </div>
     );
