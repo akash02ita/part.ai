1d87051bbd7db4e2ebcce6c21767769239a65cb5
Akashdeep Singh <akash02ita@gmail.com>
2023-02-18 19:58:41 -0700

    client: search functionality modified card component moved data.json file with typo fixes

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,12 +1,22 @@
 import React from 'react'
 
 
-const Card = ({data}) => {
-
-    const {name} = data;
+const Card = (props) => {
+  const name = props.name;
+  const { theme, description, bannerURL, address, attending, start, end } = props.values;
   return (
-    <div className='card'>
-        <h1>{name}</h1>
+    <div className="card" style={{backgroundImage: `url(${bannerURL})`}}>
+      <div>Event name: {name}</div>
+
+      {/* <img src={bannerURL} /> */}
+
+      <div>Address: {address}</div>
+      <div>{attending.length}</div>
+
+      <div>
+        <div> Starts {start} </div>
+        <div> Ends {start} </div>
+      </div>
     </div>
   )
 }
